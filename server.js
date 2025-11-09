ea064798f817ffa4a918fd596943e1ad1bf53eea
HarshMaht02004 <135724063+HarshMaht02004@users.noreply.github.com>
2025-11-09 15:03:37 +0530

    Add root route to respond with greeting
    Added a root route that responds with a greeting.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
 app.use('/api/wacc', waccRouter);
 
 const PORT = process.env.PORT || 5000;
